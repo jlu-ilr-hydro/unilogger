04b0230a605e64cabf3d15bae219363aecb6037d
Philipp Kraft <philipp.kraft@envr.jlug.de>
2022-06-22 19:12:57 +0200

    Added a not working HDFlogger and made it a package

--- unilogger/HDFlogger.py
+++ unilogger/HDFlogger.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
 class HDFLogger:
-    def __init__(self, filename, hdf_key='data', openmode='a'):
-        self.store = pd.HDFStore(filename, openmode)
+    def __init__(self, filename, hdf_key='data'):
+        # self.store = pd.HDFStore(filename)
+        self.filename=filename
         self.key = hdf_key
 
     def __call__(self, values, variableprefix=''):
         df = pd.DataFrame([v.__asdict__() for v in values])
-        self.store.put(self.key, df, append=True)
-        self.store.flush()
+        df.to_hdf(self.filename, self.key, append=True, index=False, data_columns=True)
 
     def close(self):
-        self.store.close()
+        ...
+        # self.store.close()
 
     def __enter__(self):
         return self
